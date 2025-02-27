6e0c57759191bda82c9ef317405a6594b748ac03
louai20 <alayoubi@kth.se>
2025-02-27 18:12:11 +0100

    Update layout.tsx

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       <body className={ibmPlexArabic.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
